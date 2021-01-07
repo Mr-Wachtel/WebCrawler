96785623c15a0ecc0c3b482cfb2ab9515c8d81f7
Philipp Aigner-Mühler <philipp.aigner-muehler@stud.fh-campuswien.ac.at>
2021-01-07 12:02:19 +0100

    UI angepasst. erste Tests erfolgreich! Klick den Button :P  v.1.1 -- p.aigner

--- src/main/java/WebCrawlerFX/Oberflaeche.java
+++ src/main/java/WebCrawlerFX/Oberflaeche.java
@@ -2,6 +2,7 @@ package WebCrawlerFX;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 
 import java.io.IOException;
@@ -12,10 +13,19 @@ public class Oberflaeche {
     private CheckBox checkbox1;
 
     @FXML
-    void clickcheckbox1(ActionEvent event) throws IOException {
+    private Button Button_MixUp;
+
+    @FXML
+    void Button_MixUP(ActionEvent event) throws IOException {
 
         Phil.mixUpMyCaptions(Sandra.headlines());
 
     }
 
+    @FXML
+    void clickcheckbox1(ActionEvent event) {
+
+    }
+
 }
+
