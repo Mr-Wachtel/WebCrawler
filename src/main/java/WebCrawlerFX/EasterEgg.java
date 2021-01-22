509ddb74473aab766329abd4b6223c855450b7f5
Philipp Aigner-Mühler <philipp.aigner-muehler@stud.fh-campuswien.ac.at>
2021-01-22 14:58:48 +0100

    Bernie

---
+++ src/main/java/WebCrawlerFX/EasterEgg.java
@@ -0,0 +1,11 @@
+package WebCrawlerFX;
+
+public class EasterEgg {
+
+
+    String headline ="Sei gegr\u00FC\u00DFt neugieriger Wanderer!";
+
+    String EasterEgg = "Ein kleiner Gru\u00DF von uns :)";
+
+
+}
